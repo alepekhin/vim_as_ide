d7b82387383bcdc6f2668e4c33539f1de586f8d3
Alex <alepekhine@gmail.com>
2026-06-25 12:15:17 +0300

    buffer manager added

--- config/nvim/lua/core/keymap.lua
+++ config/nvim/lua/core/keymap.lua
@@ -10,39 +10,28 @@ vim.o.softtabstop = 4
 vim.cmd('colorscheme desert')
 vim.opt.completeopt = { "menu", "menuone", "noselect", "popup" }
 
+-- back to normal mode from terminal
+vim.cmd('tnoremap <Esc> <C-\\><C-n>')
+
+vim.keymap.set('i', '<C-p><C-p>', '<C-\\><C-n>', { noremap = true, silent = true, desc = 'Back to normal mode' })
 vim.keymap.set('n', '<Leader><Leader>', '<C-w><C-w>', { noremap = true, silent = true, desc = 'Next window' })
-vim.keymap.set('n', '<Leader>ja', ':lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true, desc = 'jls: code action' })
-vim.keymap.set('n', '<Leader>b', ':lua require("opencode").toggle()<CR>', { noremap = true, silent = true, desc = 'opencode: toggle' })
-vim.keymap.set('n', '<Leader>jd', '<C-]>', { noremap = true, silent = true, desc = 'jls: Go to declaration' })
-vim.keymap.set('n', '<Leader>e', ':lua require("opencode").select()<CR>', { noremap = true, silent = true, desc = 'opencode: menu' })
-vim.keymap.set('n', '<Leader>ji', ':lua vim.lsp.buf.implementation()<CR>', { noremap = true, silent = true, desc = 'jls: go to implementation' })
-vim.keymap.set('n', '<Leader>js', ':lua vim.lsp.buf.signature_help()<CR>', { noremap = true, silent = true, desc = 'jls: signature help' })
-vim.keymap.set('n', '<Leader>jh', ':lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true, desc = 'jls: hover' })
-vim.keymap.set('n', '<Leader>jr', ':lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true, desc = 'jls: rename' })
 vim.keymap.set('n', '<Leader>t', ':new term://bash<CR>', { noremap = true, silent = true, desc = 'Open terminal' })
-vim.keymap.set('n', '<Leader>dn', ':DapNew<CR>', { noremap = true, silent = true, desc = 'dap: new' })
-vim.keymap.set('n', '<Leader>db', ':DapToggleBreakpoint<CR>', { noremap = true, silent = true, desc = 'dap: toggle breakpoint' })
-vim.keymap.set('n', '<Leader>do', ':lua require("dapui").open()<CR>', { noremap = true, silent = true, desc = 'dap: open dapui' })
-vim.keymap.set('n', '<Leader>dc', ':lua require("dapui").close()<CR>', { noremap = true, silent = true, desc = 'dap: close dapui' })
+vim.keymap.set('n', '<Leader>a', ':lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true, desc = 'jls: code action' })
+vim.keymap.set('n', '<Leader>b', ':lua vim.lsp.buf.implementation()<CR>', { noremap = true, silent = true, desc = 'jls: go to implementation' })
+vim.keymap.set('n', '<Leader>c', ':lua vim.lsp.buf.signature_help()<CR>', { noremap = true, silent = true, desc = 'jls: signature help' })
+vim.keymap.set('n', '<Leader>d', '<C-]>', { noremap = true, silent = true, desc = 'jls: Go to declaration' })
+vim.keymap.set('n', '<Leader>e', ':lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true, desc = 'jls: hover' })
+vim.keymap.set('n', '<Leader>f', ':lua vim.lsp.buf.rename()<CR>', { noremap = true, silent = true, desc = 'jls: rename' })
+vim.keymap.set('n', '<Leader>g', ':lua require("opencode").toggle()<CR>', { noremap = true, silent = true, desc = 'opencode: toggle' })
+vim.keymap.set('n', '<Leader>i', ':lua require("opencode").select()<CR>', { noremap = true, silent = true, desc = 'opencode: menu' })
+vim.keymap.set('n', '<Leader>j', ':DapNew<CR>', { noremap = true, silent = true, desc = 'dap: new' })
+vim.keymap.set('n', '<Leader>k', ':DapToggleBreakpoint<CR>', { noremap = true, silent = true, desc = 'dap: toggle breakpoint' })
+vim.keymap.set('n', '<Leader>l', ':lua require("dapui").open()<CR>', { noremap = true, silent = true, desc = 'dap: open dapui' })
+vim.keymap.set('n', '<Leader>m', ':lua require("dapui").close()<CR>', { noremap = true, silent = true, desc = 'dap: close dapui' })
+vim.keymap.set('n', '<Leader>n', ':lua require("buffer_manager.ui").toggle_quick_menu()<CR>', { noremap = true, silent = true, desc = 'buffer_manager:ui' })
 
 -- my key help
 vim.keymap.set('n', '<Leader>k', ':new +set\\ noma|set\\ ro ~/.config/nvim/keys<CR>', { noremap = true, silent = true, desc = 'Open keys file' })
 
-
--- Execute Ctrl-N after 3 symbols printed
-vim.api.nvim_create_autocmd("InsertCharPre", {
-    callback = function()
-        -- Get current line content up to cursor
-        local line = vim.api.nvim_get_current_line()
-        local col = vim.api.nvim_win_get_cursor(0)[2]
-
-        -- Check if we have 3 characters in the current word
-        local current_word = string.match(line:sub(1, col), "%S+$") or ""
-        if #current_word == 2 then -- Using 2 because the 3rd char is about to be added
-            -- Feed Ctrl-N after the current character is inserted
-            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-N>", true, false, true), "m", true)
-        end
-    end
-})
 
 
---
+++ config/nvim/lua/plugins/buffer-manager.lua
@@ -0,0 +1,5 @@
+vim.pack.add({
+        'https://github.com/nvim-lua/plenary.nvim',
+        'https://github.com/j-morano/buffer_manager.nvim',
+})
+
